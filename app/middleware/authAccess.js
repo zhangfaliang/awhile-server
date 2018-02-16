1dfccfa2ef0f602e434bbd5af1531711fa782ef3
devmsg <co.devmsg@gmail.com>
2018-02-16 09:49:32 +0800

    中间件处理

--- app/middleware/authAccess.js
+++ app/middleware/authAccess.js
@@ -4,7 +4,7 @@ module.exports = (options, app) => {
   return async (ctx, next) => {
     const getCurrentUserAuthAccess = await ctx.service.authAccess.getCurrentUserAuthAccess();
     const getCurrentUser = await ctx.service.user.getCurrentUser();
-    if (findIndex(getCurrentUserAuthAccess, item => item.action === ctx.url) > -1 || getCurrentUser.id === 1) {
+    if (findIndex(getCurrentUserAuthAccess, item => item.action === ctx.url) > -1 || getCurrentUser.type === 1) {
       await next();
     } else {
       ctx.body = {
